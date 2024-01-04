a033ee45f36ac4b354b24abbb01527e3e374c033
aong13 <oaoa13@pukyong.ac.kr>
2024-01-04 19:12:14 +0900

    routing sticker and mypage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sticker from './page/my_page';
+import Sticker from './page/sticker';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="root-wrap">
+    <BrowserRouter>
+    <Header />
+      <Routes>
+      <Route path="/sticker" element={<Sticker />} />
+      <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+  );
 }
 
 export default App;
