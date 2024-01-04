a99cb2a0b386a9dac51476093805c37fa02aa71a
lookyoung0605 <dlfnrud0605@gmail.com>
2024-01-04 19:58:44 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Sticker from './page/my_page';
-import MyPage from './page/sticker';
+import Stickerpage from "./page/Stikerpage";
+import MyPage from "./page/my_page";
 
 function App() {
   return (
     <div className="root-wrap">
-    <BrowserRouter>
-      <Routes>
-      <Route path="/sticker" element={<Sticker />} />
-      <Route path="/mypage" element={<MyPage />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/stickerpage" element={<Stickerpage />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
