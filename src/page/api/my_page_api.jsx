15fc71d1becaaaa81e2be1936557fa496c1a50f2
aong13 <oaoa13@pukyong.ac.kr>
2024-01-05 07:24:17 +0900

    mypage/1

--- src/page/api/my_page_api.jsx
+++ src/page/api/my_page_api.jsx
@@ -2,7 +2,7 @@ import AxiosInstance from "./AxiosInstance";
 
 const MyPageApi = async () => {
   try {
-    const response = await AxiosInstance.get("/MyPage");
+    const response = await AxiosInstance.get("/MyPage/1");
 
     console.log(response.data);
 
