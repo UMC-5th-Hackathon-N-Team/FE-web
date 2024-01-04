74c08ba4e5a261dc7961208a14d7401f53ceb77b
lookyoung0605 <dlfnrud0605@gmail.com>
2024-01-05 03:44:45 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import Stickerpage from "./page/Stikerpage";
 import MyPage from "./page/my_page";
 import Progression from "./page/progression";
 import Gift from "./page/Gift";
-import A from "./page/A";
 function App() {
   return (
     <div className="root-wrap">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Gift />} />
-
+          <Route path="/gift" element={<Gift />} />
           <Route path="/stickerpage" element={<Stickerpage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/progression" element={<Progression />} />
